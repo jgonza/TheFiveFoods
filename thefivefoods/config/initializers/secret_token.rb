08224b203a0eec0920e75f134eec98f8594430c0
jrgonzalez <jgonzalez@timbasoftware.com>
2012-05-25 17:50:19 -0300

    First commit

---
+++ thefivefoods/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thefivefoods::Application.config.secret_token = 'e80e9ac823f2da937ec75c6e6bc15cf5ee05266f4eba42a0798d581eafec34800b3310782f8fdc43359470aa69305196455c2c63fd5da9e21c6ad627e19c2ef0'
